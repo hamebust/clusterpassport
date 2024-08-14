4c6d37619b4ff137a4cae519f76d628856412910
Henry Medina <55643332+hamebust@users.noreply.github.com>
2024-08-13 20:00:50 -0400

    20240813-20:00

--- lib/features/app/splash/splash_screen.dart
+++ lib/features/app/splash/splash_screen.dart
@@ -1,98 +1,88 @@
-//Paquete que permite conectar a dart:async para hacer un delay en la pantalla de presentación
-//Package that allows connecting to dart:async to make a delay in the presentation screen
+// Paquete que permite conectar a dart:async para hacer un delay en la pantalla de presentación
 import 'dart:async';
 
-//Paquete que permite conectar a hoja de estilos de la aplicación en la carpeta lib/features/app/theme/style
-//Package that allows connecting to the style sheet of the application in the lib/features/app/theme/style folder
+// Paquete que permite conectar a hoja de estilos de la aplicación en la carpeta lib/features/app/theme/style
 import 'package:cluster_passport/features/app/theme/style.dart';
 
-//Paquete que permite conectar a WelcomePage: página de bienvenida en la carpeta lib/features/app/welcome
-//Package that allows connecting to WelcomePage: welcome page in the lib/features/app/welcome folder
+// Paquete que permite conectar a WelcomePage: página de bienvenida en la carpeta lib/features/app/welcome
 import 'package:cluster_passport/features/app/welcome/welcome_page.dart';
 
-//Paquete que permite conectar a Flutter: widget principal de la aplicación en la carpeta main.dart
-//Package that allows connecting to Flutter: main widget of the application in main.dart
+// Paquete que permite conectar a Flutter: widget principal de la aplicación en la carpeta main.dart
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  // ignorar: library_private_types_in_public_api
-  // ignore: library_private_types_in_public_api
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-//Clase que contiene el estado del widget SplashScreen que permite un delay en la pantalla de presentación
-//Class that contains the state of the SplashScreen widget that allows a delay in the presentation screen
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
+    super.initState();
+    _navigateToWelcomePage();
+  }
+
+  void _navigateToWelcomePage() {
     Timer(const Duration(seconds: 2), () {
       if (mounted) {
-        Navigator.pushAndRemoveUntil(
+        Navigator.pushReplacement(
           context,
           MaterialPageRoute(
             builder: (_) => const WelcomePage(),
           ),
-          (route) => false,
         );
       }
     });
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: backgroundColor,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(),
-            Image.asset(
-              "assets/whats_app_logo.png",
-              color: Colors.white,
-              width: 100,
-              height: 100,
-            ),
-            Column(
-              children: [
-                Text(
-                  "from",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                      color: greyColor.withOpacity(.6)),
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(),
+          Image.asset(
+            "assets/whats_app_logo.png",
+            color: Colors.white,
+            width: 100,
+            height: 100,
+          ),
+          Column(
+            children: [
+              Text(
+                "from",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: greyColor.withOpacity(.6),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      "assets/meta.png",
-                      color: Colors.white,
-                      width: 35,
-                      height: 35,
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    const Text(
-                      "Meta",
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-              ],
-            )
-          ],
-        ));
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/meta.png",
+                    color: Colors.white,
+                    width: 35,
+                    height: 35,
+                  ),
+                  const SizedBox(width: 5),
+                  const Text(
+                    "Meta",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,45 @@
-//ClusterPasport. Aplicación de gestión de ingresos de personas a Instalaciones
-//ClusterPassport. Application of person ingress management to Installations
+// ClusterPasport. Aplicación de gestión de ingresos de personas a Instalaciones
+// ClusterPassport. Application of person ingress management to Installations
 
 // Todos los notas son en español e inglés
 // All notes are in spanish and english
 
-  //Paquete que permite conectar a SplashScreen: vista de presentación de la aplicación en la carpeta lib/features/app/splash
-  //Package that allows connecting to SplashScreen: presentation of the application in the lib/features/app/splash folder
-  import 'package:cluster_passport/features/app/splash/splash_screen.dart';
-  //Paquete que permite conectar a Theme: tema de la aplicación en la carpeta lib/features/app/theme
-  //Package that allows connecting to Theme: theme of the application in the lib/features/app/theme folder
-  import 'package:cluster_passport/features/app/theme/app_theme.dart';
-  //Paquete que permite conectar a S: internacionalización de la aplicación en la carpeta lib/generated/l10n
-  //Package that allows connecting to S: internationalization of the application in the lib/generated/l10n folder
-  import 'package:cluster_passport/generated/l10n.dart';
-  //Paquete que permite conectar a OnGenerateRoute: generador de rutas de la aplicación en la carpeta routes/on_generate_routes
-  //Package that allows connecting to OnGenerateRoute: route generator of the application in the routes/on_generate_routes folder
-  import 'package:cluster_passport/routes/on_generate_routes.dart';
-  //Paquete que permite conectar a MaterialApp: widget principal de la aplicación en la carpeta main.dart
-  //Package that allows connecting to MaterialApp: main widget of the application in main.dart
-  import 'package:flutter/material.dart';
-  //Paquete que permite conectar a FlutterLocalization: internacionalización de Flutter en la carpeta flutter_localizations
-  //Package that allows connecting to FlutterLocalization: internationalization of Flutter in the flutter_localizations folder
-  import 'package:flutter_localizations/flutter_localizations.dart';
 
-  //import 'package:flutter_modular/flutter_modular.dart';
-  //import 'package:cluster_passport/app_modular/app_modular.dart';
+import 'package:cluster_passport/features/app/theme/app_theme.dart';
+import 'package:cluster_passport/generated/l10n.dart';
+import 'package:cluster_passport/routes/on_generate_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
-  //Ejecución de la aplicación Cluster Passport
-  //Execution of Cluster Passport application
+  // Ejecución de la aplicación Cluster Passport
+  // Execution of Cluster Passport application
   runApp(const ClusterPassport());
-//  runApp(ModularApp(module: AppModule(), child: const ClusterPassport()));
 }
 
 // Clase principal de la aplicación
 // Main application class
-// Clase ClusterPassport que extiende un widget sin estado
-// ClusterPassport class that extends a StatelessWidget without state
 class ClusterPassport extends StatelessWidget {
   const ClusterPassport({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        //Titulo de la aplicación
-        //App title
-        title: 'Cluster Passport',
-
-        //Paquete de internacionalización de Flutter localización
-        //Flutter localization package
-        localizationsDelegates: const [
-          //La clase S que se encuentra en el archivo l10n.dart
-          //The S class that is found in l10n.dart
-          //Los archivos de traducción se encuentran en la carpeta lib/l10n
-          //The translation files are found in the lib/l10n folder
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-
-        //Idioma de la aplicación:  esta sección configura el lenguaje en el que se muestra la app
-        //App language: this section configures the language in which the app is displayed
-        //Idioma actual Español
-        //Current language Spanish
-        locale: const Locale('es'),
-
-        //Tema de la aplicación
-        //App theme
-        debugShowCheckedModeBanner: false,
-        //Tema claro: esta sección configura el tema claro de la app
-        //This section configures the light theme of the app
-        theme: AppTheme.lightTheme,
-        //Tema oscuro: esta sección configura el tema oscuro de la app
-        //This section configures the dark theme of the app
-        darkTheme: AppTheme.darkTheme,
-        //Modo de tema: esta sección configura el modo de tema de la app
-        //This section configures the theme mode of the app
-        themeMode: ThemeMode.system,
-
-        //Rutas de la aplicación
-        //App routes
-        //Ruta inicial: esta sección configura la ruta inicial de la app
-        //This section configures the initial route of the app
-        initialRoute: "/",
-        //Generador de rutas: esta sección configura el generador de rutas de la app
-        //This section configures the route generator of the app
-        onGenerateRoute: OnGenerateRoute().route,
-        //Rutas: esta sección configura las rutas de la app
-        //This section configures the routes of the app
-        routes: {
-          '/': (context) => const SplashScreen(),
-        });
+      title: 'Cluster Passport',
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: const Locale('es'),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      initialRoute: "/",
+      onGenerateRoute: OnGenerateRoute().route,
+    );
   }
 }
